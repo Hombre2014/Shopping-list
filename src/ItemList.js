ff83a0536d6b430664216ca3674fa2452c970c59
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-06-02 15:37:19 +0200

    Implemented the whole app including add and search items

---
+++ src/ItemList.js
@@ -0,0 +1,18 @@
+import LineItem from './LineItem';
+
+const ItemList = ({ items, handleCheck, handleDelete }) => {
+  return (
+    <ul>
+      {items.map((item) => (
+        <LineItem
+          item={item}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+          key={item.id}
+        />
+      ))}
+    </ul>
+  )
+}
+
+export default ItemList;
